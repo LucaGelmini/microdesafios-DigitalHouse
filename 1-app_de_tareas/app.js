2ef39eafa33eb4673814b618b9c775cd607fa2e4
Luca <lucagelmini@github.com>
2022-06-23 18:42:20 -0300

    casi casi, falta el delete

--- 1-app_de_tareas/app.js
+++ 1-app_de_tareas/app.js
@@ -16,45 +16,99 @@ const {availableOptionsTemplate, actionRequired, unrecognisedCommand} = require(
 //     return false;
 //   }
 
-function mainSwitch(data, action, rl){
+async function mainSwitch(data, action, rl){
     switch (action){
-            case 'listar':
-                tareas.listAllTasks(data)
-                .then(()=>{
+        case 'opciones':
+        case '?':
+        case 'ayuda':
+            console.clear();
+            console.log(availableOptionsTemplate());
+            rl.close();
+            cli();
+            break;
+        case 'listar':
+            tareas.listAllTasks(data)
+            .then(()=>{
+                rl.close();
+                cli();
+            })
+        case 'nueva':
+            rl.question('Inserte el nombre de la tarea: ', action1 =>{
+                const nombre = action1;
+                rl.question('Inserte el estado de la tarea: ', action2 =>{
+                    const estado = action2;
+                    const nuevaTarea = tareas.newTask(nombre, estado);
+                    data =  tareas.appendTask(data, nuevaTarea);
+                    console.log('\n')
+                    console.clear();
                     rl.close();
                     cli();
                 })
-            case 'nueva':
-                rl.question('Inserte el nombre de la tarea: ', action1 =>{
-                    const nombre = action1;
-                    rl.question('Inserte el estado de la tarea: ', action2 =>{
-                        const estado = action2;
-                        const nuevaTarea = tareas.newTask(nombre, estado);
-                        data =  tareas.appendTask(data, nuevaTarea);
-                        console.log('\n')
+            })
+            break;
+        case 'guardar':
+            tareas.save(data);
+            console.clear();
+            rl.close();
+            cli();
+            break;
+        case 'buscar':
+            const busqueda = () =>{
+                rl.question('Ingrese el nombre de la tarea: ', async action3 =>{
+                    await tareas.search(data, action3);
+                    rl.question('¿Ir atras? [S/N]: ', action =>{
+                        if( action === 'N'){
+                            console.log(action)
+                            busqueda();
+                        }else{
+                            console.clear();
+                            rl.close();
+                            cli();
+                        }
+                    })
+                })
+            }
+            busqueda()
+            break;
+        case 'editar':
+            const busquedaEditar = () =>{
+                rl.question('Ingrese el nombre de la tarea a editar: ', async action4 =>{
+                    const target = await tareas.search(data, action4);
+                    rl.question('Ingrese nuevo estado: ', action => {
+                        data = tareas.patch(data, target, action);
                         console.clear();
                         rl.close();
                         cli();
                     })
+                    
                 })
-                break;
-            case 'guardar':
-                
-    
-            case 'salir':
-                rl.close();
-                break;
-            default:
-                unrecognisedCommand();
-                console.clear();
-                rl.close();
-                cli();
+            }
+            busquedaEditar()
+            break;
+        case 'eliminar':
+            const busquedaEliminar = () =>{
+                rl.question('Ingrese el nombre de la tarea a eliminar: ', async action5 =>{
+                    const target = await tareas.search(data, action5);
+                    data = tareas.delete(data, target);
+                    // console.clear();
+                    rl.close();
+                    cli();
+                })
+            }
+            busquedaEliminar()
+            break;
+        case 'salir':
+            rl.close();
+            break;
+        default:
+            console.clear();
+            unrecognisedCommand();
+            rl.close();
+            cli();
     }
+    console.log('afuera', await data)
+    return data
 }
-
-
-let data = tareas.read()
-// argv[2] == undefined ? actionRequired() : data = mainSwitch(data)
 function cli(){
     const rl =readline.createInterface({
         input: process.stdin,
@@ -66,17 +120,14 @@ function cli(){
             actionRequired();
             cli();
         }else{
-            mainSwitch(data, action, rl)
+            data = mainSwitch(data, action, rl)
 
         }
     })
 
 }
+
+console.clear();
+let data = tareas.read()
  cli()
 
-
-// readline.question(`What's your name?`, name => {
-//     tareas.listAllTasks(data)
-//     console.log(`Hi ${name}!`);
-//     readline.close();
-// });
--- 1-app_de_tareas/utils/funcionesDeTareas.js
+++ 1-app_de_tareas/utils/funcionesDeTareas.js
@@ -31,10 +31,45 @@ const funcionesDeTareas = {
         const data = await dataPromisse;
         data.push(newTask)
         return data
+    },
+    save: async function(dataPromisse){
+        const data = await dataPromisse;
+        await fs.writeFile('./data/tareas.json', JSON.stringify(data));
+    },
+    search: async function(dataPromisse, title){
+        let data = await dataPromisse;
+        data = data.map((task, idx) => {
+            return {
+                ...task,
+                indice: idx
+            }
+        });
+        const targetTasks = data.filter((tarea)=> (tarea.titulo == title));
+        targetTasks.length >0 ?
+        targetTasks.forEach(targetTask=>console.log(`%cTarea número ${targetTask.indice+1}. Estado: ${targetTask.estado}\n`, color='green')) :
+        console.log(`%cNo existen tareas que coincidan`, color='red');
+        return targetTasks
+    },
+    patch: async function(dataPromisse,targetTask, newState){
+        let data = await dataPromisse;
+        if (targetTask.length != 1){
+            console.log('Su búsqueda coincide con varias tareas, debe elegir una tarea')
+            return data
+        }
+        targetTask = targetTask[0]
+        data[targetTask.indice] = {titulo:data[targetTask.indice].titulo, estado:newState}
+        return data
+    },
+    delete: async function(dataPromisse, targetTask){
+        let data = await dataPromisse;
+        if (targetTask.length != 1){
+            console.log('Su búsqueda coincide con varias tareas, debe elegir una tarea')
+            return data
+        }
+        targetTask = targetTask[0]
+        data = data.filter((task, idx)=>idx!=targetTask.indice)
+        return data
     }
-
-        
-
 
 }
 module.exports = funcionesDeTareas
